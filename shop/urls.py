290ace42780c7d6d4118dfa4b1723f01b3ff9035
Ali Ganji <aliganji142879@gmail.com>
2024-08-05 16:20:44 +0330

    nested categories

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,5 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='product_list'),
     path('<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('category/<slug:category_slug>/', views.ProductListView.as_view(), name='category_filter'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 
-from .models import Product
+from .models import Product, Category
 
 
 class ProductListView(View):
-    def get(self, request):
+    def get(self, request, category_slug=None):
         products = Product.objects.filter(available=True)
-        return render(request, 'shop/list.html', {'products': products})
+        categories = Category.objects.filter(is_sub=False)
+        if category_slug:
+            category = Category.objects.get(slug=category_slug)
+            products = products.filter(category=category)
+        return render(request, 'shop/list.html', {'products': products, 'categories': categories})
 
 
 class ProductDetailView(View):
