d22b3250635e6f67462de4f4081ae87a694cac35
Ali Ganji <aliganji142879@gmail.com>
2024-08-05 18:45:12 +0330

    cart add,detail,remove

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+
+from shop.models import Product
+from .cart import Cart
+from .forms import CartAddForm
 
 
 class CartView(View):
     def get(self, request):
-        return render(request, 'orders/cart.html')
+        cart = Cart(request)
+        return render(request, 'orders/cart.html', {'cart': cart})
 
 
 class CartAddView(View):
     def post(self, request, product_id):
-        pass
+        cart = Cart(request)
+        product = get_object_or_404(Product, pk=product_id)
+        form = CartAddForm(request.POST)
+        if form.is_valid():
+            cart.add(product, form.cleaned_data['quantity'])
+        return redirect('orders:cart')
+
+
+class CartRemoveView(View):
+    def get(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        cart.remove(product)
+        return redirect('orders:cart')
